3a90cf8375fbd6548586e83723408edf1bfcac3e
nmsimons <nick@dreamlarge.com>
2025-03-17 15:48:41 -0700

    simplify selection

--- src/utils/presence_helpers.ts
+++ src/utils/presence_helpers.ts
@@ -5,7 +5,7 @@
 
 import {
 	type IPresence,
-	Latest as defineState,
+	Latest as psf_latest,
 	type PresenceStatesSchema,
 	type PresenceStatesEntries,
 	LatestMap,
@@ -15,11 +15,10 @@ import { FeltShape } from "./shapes.js";
 export class SelectionManager {
 	statesName: `${string}:${string}` = "shape:selection";
 
-	selectedType = defineState({ items: [] as string[] });
-
+	selectionSchema: string[] = [];
 	statesSchema = {
 		// sets selected to an array of strings
-		selected: this.selectedType,
+		selected: psf_latest(this.selectionSchema),
 	} satisfies PresenceStatesSchema;
 
 	statesSchema2 = {
@@ -27,20 +26,20 @@ export class SelectionManager {
 		selected: LatestMap({ items: [] as number[] }),
 	} satisfies PresenceStatesSchema;
 
-	private selectionState: PresenceStatesEntries<typeof this.statesSchema>["selected"];
+	private presenceStates: PresenceStatesEntries<typeof this.statesSchema>["selected"];
 	private presence: IPresence;
 
 	constructor(presence: IPresence) {
-		this.selectionState = presence.getStates(this.statesName, this.statesSchema).props.selected;
+		this.presenceStates = presence.getStates(this.statesName, this.statesSchema).props.selected;
 		this.presence = presence;
 	}
 
 	public events = {
 		onRemoteUpdate: (callback: () => void) => {
-			return this.selectionState.events.on("updated", callback);
+			return this.presenceStates.events.on("updated", callback);
 		},
 		onLocalUpdate: (callback: () => void) => {
-			return this.selectionState.events.on("localUpdated", callback);
+			return this.presenceStates.events.on("localUpdated", callback);
 		},
 		onAttendeeJoined: (callback: () => void) => {
 			return this.presence.events.on("attendeeJoined", callback);
@@ -52,16 +51,16 @@ export class SelectionManager {
 
 	/** Test if the given id is selected by the local client */
 	public testSelection(id: string) {
-		return this.selectionState.local.items.indexOf(id) != -1;
+		return this.presenceStates.local.indexOf(id) != -1;
 	}
 
 	/** Test if the given id is selected by any remote client */
 	public testRemoteSelection(id: string) {
 		const remoteSelectedClients: string[] = [];
 
-		for (const cv of this.selectionState.clientValues()) {
+		for (const cv of this.presenceStates.clientValues()) {
 			if (cv.client.getConnectionStatus() === "Connected") {
-				if (cv.value.items.indexOf(id) !== -1) {
+				if (cv.value.indexOf(id) !== -1) {
 					remoteSelectedClients.push(cv.client.sessionId);
 				}
 			}
@@ -72,7 +71,7 @@ export class SelectionManager {
 
 	/** Clear the current selection */
 	public clearSelection() {
-		this.selectionState.local = { items: [] };
+		this.presenceStates.local = [];
 	}
 
 	/** Change the selection to the given id or array of ids */
@@ -80,7 +79,7 @@ export class SelectionManager {
 		if (typeof id == "string") {
 			id = [id];
 		}
-		this.selectionState.local = { items: id };
+		this.presenceStates.local = id;
 	}
 
 	/** Toggle the selection of the given id */
@@ -96,35 +95,35 @@ export class SelectionManager {
 
 	/** Add the given id to the selection */
 	public addToSelection(id: string) {
-		const arr: string[] = this.selectionState.local.items.slice();
+		const arr: string[] = this.presenceStates.local.slice();
 		const i = arr.indexOf(id);
 		if (i == -1) {
 			arr.push(id);
 		}
-		this.selectionState.local = { items: arr };
+		this.presenceStates.local = arr;
 	}
 
 	/** Remove the given id from the selection */
 	public removeFromSelection(id: string) {
-		const arr: string[] = this.selectionState.local.items.slice();
+		const arr: string[] = this.presenceStates.local.slice();
 		const i = arr.indexOf(id);
 		if (i != -1) {
 			arr.splice(i, 1);
 		}
-		this.selectionState.local = { items: arr };
+		this.presenceStates.local = arr;
 	}
 
 	/** Get the current local selection array */
 	public getLocalSelected(): readonly string[] {
-		return this.selectionState.local.items;
+		return this.presenceStates.local;
 	}
 
 	/** Get the current remote selection map where the key is the selected item and the value is an array of client ids */
 	public getRemoteSelected(): Map<string, string[]> {
 		const remoteSelected = new Map<string, string[]>();
-		for (const cv of this.selectionState.clientValues()) {
+		for (const cv of this.presenceStates.clientValues()) {
 			if (cv.client.getConnectionStatus() === "Connected") {
-				for (const id of cv.value.items) {
+				for (const id of cv.value) {
 					if (!remoteSelected.has(id)) {
 						remoteSelected.set(id, []);
 					}
@@ -140,13 +139,15 @@ export class SelectionManager {
 export class DragManager {
 	statesName: `${string}:${string}` = "shape:dragging";
 
+	dragSchema: DragPackage = {
+		id: null,
+		x: 0,
+		y: 0,
+	};
+
 	statesSchema = {
 		// sets dragging to an object with id, x, and y properties
-		dragging: defineState({
-			id: null,
-			x: 0,
-			y: 0,
-		} as DragPackage),
+		dragging: psf_latest(this.dragSchema),
 	} satisfies PresenceStatesSchema;
 
 	private dragState: PresenceStatesEntries<typeof this.statesSchema>["dragging"];
